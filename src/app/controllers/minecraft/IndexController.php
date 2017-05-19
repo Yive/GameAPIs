296b6553eb61f5eb826d8fbbfe41e61fd768b919
Yive <admin@yive.me>
2017-05-19 06:38:35 -0700

    Start Minecraft APIs

---
+++ src/app/controllers/minecraft/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+class SupportedController extends ControllerBase {
+
+    public function initialize() {
+        $this->tag->setTitle("GameAPIs");
+    }
+
+    public function indexAction() {
+
+    }
+
+    public function minecraftAction() {
+        $this->tag->prependTitle("Minecraft APIs - ");
+    }
+
+}
