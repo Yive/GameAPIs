ab9c38c4886fff0942cee87406467d3f8c739a87
Yive <admin@yive.me>
2017-05-21 22:24:35 -0700

    Switch to Bulma for CSS

--- src/app/config/routes.php
+++ src/app/config/routes.php
@@ -16,6 +16,36 @@ $router->add('/supported/:action', [
     'action'        => 1
 ]);
 
+$router->add('/supported/minecraft/:action', [
+    'namespace'     => 'GameAPIs\Controllers\Supported',
+    'controller'    => 'minecraft',
+    'action'        => 1
+]);
+
+$router->add('/supported/minecraft/query/:action', [
+    'namespace'     => 'GameAPIs\Controllers\Supported\Minecraft',
+    'controller'    => 'query',
+    'action'        => 1
+]);
+
+$router->add('/supported/minecraft/images/:action', [
+    'namespace'     => 'GameAPIs\Controllers\Supported\Minecraft',
+    'controller'    => 'images',
+    'action'        => 1
+]);
+
+$router->add('/supported/minecraft/ecommerce/:action', [
+    'namespace'     => 'GameAPIs\Controllers\Supported\Minecraft',
+    'controller'    => 'ecommerce',
+    'action'        => 1
+]);
+
+$router->add('/supported/minecraft/extra/:action', [
+    'namespace'     => 'GameAPIs\Controllers\Supported\Minecraft',
+    'controller'    => 'extra',
+    'action'        => 1
+]);
+
 $router->notFound([
     'namespace'     => 'GameAPIs\Controllers',
     'controller'    => 'index',
--- src/app/controllers/Supported/MinecraftController.php
+++ src/app/controllers/Supported/MinecraftController.php
@@ -2,7 +2,7 @@
 
 namespace GameAPIs\Controllers\Supported;
 
-class IndexController extends ControllerBase {
+class MinecraftController extends ControllerBase {
 
     public function initialize() {
         $this->tag->setTitle("GameAPIs");
@@ -10,10 +10,23 @@ class IndexController extends ControllerBase {
 
     public function indexAction() {
         $this->tag->prependTitle("Minecraft APIs - ");
+        return $this->response->redirect("supported/minecraft");
     }
 
     public function queryAction() {
         $this->tag->prependTitle("Minecraft Server Query API - ");
+    }
+
+    public function playerAction() {
+        $this->tag->prependTitle("Minecraft Player Images API - ");
+    }
+
+    public function ecommerceAction() {
+        $this->tag->prependTitle("Minecraft Server Ecommerce API - ");
+    }
+
+    public function extraAction() {
+        $this->tag->prependTitle("Minecraft Extra APIs - ");
     }
 
 }
