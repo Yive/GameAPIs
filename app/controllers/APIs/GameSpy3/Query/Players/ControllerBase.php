5ad38ce034c22fe7153d6232c124494289db3f75
Yive <admin@yive.me>
2017-09-08 13:38:09 -0700

    Add testing for if a server uses GameSpy3

---
+++ app/controllers/APIs/GameSpy3/Query/Players/ControllerBase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace GameAPIs\Controllers\APIs\GameSpy3\Query\Players;
+
+use Phalcon\Mvc\Controller;
+
+class ControllerBase extends Controller {
+    public function afterExecuteRoute() {
+        $this->view->disable();
+        header("Content-Type: application/json; charset=UTF-8");
+    }
+}
